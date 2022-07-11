271e712c382ef459abddfc0ebd6785f8b1eaf912
XDGamerLPLPLP <92158010+XDGamerLPLPLP@users.noreply.github.com>
2022-07-11 17:53:06 +0200

    Sidemenu Buttons added
    The Buttons in the Sidemenu are now added and aviable to change design.

--- js/nHIE_create_Buttons.js
+++ js/nHIE_create_Buttons.js
@@ -5,30 +5,11 @@ window.onload = function() {
     $.getJSON('../json/gamelist.json', function(json) {
         // console.log( "success" );
 
-
-        var margin;
-        var gameCount = Object.keys(json).length;
-        if (navigator.userAgentData.mobile) { //Margin calculator for mobile
-            margin = (100 - (gameCount * 28)) / (gameCount + 1);
-    
-            if (margin < 5)
-                margin = 5;
-
-        }
-        else { //Margin calculator for deskt
-            margin = (100 - (gameCount * 20)) / (gameCount + 1);
-    
-            if (margin < 10)
-                margin = 10;
-        }
-
-        
         $.each( json, function( name, game ) {
             var div = document.createElement('div');
             div.type = 'div';
             div.innerHTML = '<b>' + name + '</b>';
             div.className = 'div-styled';
-            div.style.marginLeft = margin + "%";
 
 
             div.onclick = function() {
