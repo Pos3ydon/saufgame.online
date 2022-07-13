21c5f07149b6804e96374e101bff2e50e38d40d6
XDGamerLPLPLP <92158010+XDGamerLPLPLP@users.noreply.github.com>
2022-07-13 18:27:00 +0200

    Changed neverHaveIEver.js
    Changed the Data Type

--- js/neverHaveIEver.js
+++ js/neverHaveIEver.js
@@ -2,7 +2,7 @@ window.onload = function() {
     $.ajax({
         url: "./../php/getQuestion_neverHaveIEver.php",
         type: "POST",
-        data: data
+        data: {}
     }).done(function(result) {
         console.log(result);
     });
