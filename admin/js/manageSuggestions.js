e0ad4930d2908a9e760291ed0e1ce6a13dcf6a71
Tobias Leitner <tobias.leitner2002@gmail.com>
2022-07-13 21:25:18 +0200

    manageSuggestions fix

--- admin/js/manageSuggestions.js
+++ admin/js/manageSuggestions.js
@@ -19,8 +19,26 @@ $(document).ready(function() {
 
                     $.each(result, function(index, suggestion) {
                         var div = document.createElement('div');
-                        div.innerHTML = "<p class='suggestionText'>" + suggestion.suggestion + "</p><button class='btn_yes'></button><button class='btn_no'></button>";
+                        div.innerHTML = "<p class='suggestionText'>" + suggestion.suggestion + "</p>";//<button class='btn_yes'></button><button class='btn_no'></button>";
                         div.className = "suggestion";
+
+                        var btn_yes = document.createElement('button');
+                        btn_yes.className = "btn_yes";
+                        btn_yes.click = function(e) {
+                            accept_suggestion(this.parentNode.children[0].innerHTML);
+                            this.parentNode.remove();
+                        }
+
+                        var btn_no = document.createElement('button');
+                        btn_no.className = "btn_no";
+                        btn_no.click = function(e) {
+                            reject_suggestion(this.parentNode.children[0].innerHTML);
+                            this.parentNode.remove();
+                        }
+
+                        div.append(btn_yes);
+                        div.append(btn_no);
+
 
                         $("#suggestionScrollDiv").append(div);
                     });
@@ -38,35 +56,57 @@ $(document).ready(function() {
 
 
 
-    $(".btn_yes").bind({
-        click: function(e) {
-            var text = this.parentNode.children[0].innerHTML;
+    // $(".btn_yes").bind({
+    //     click: function(e) {
+    //         var text = this.parentNode.children[0].innerHTML;
 
-            $.ajax({
-                url: "./../php/add_neverHaveIEver.php",
-                type: "POST",
-                data: { suggestion: text }
-            }).done(function(result) {
-                //console.log(result);
-            });
+    //         console.log("lol");
 
-            this.parentNode.remove();
-        }
-    });
+    //         $.ajax({
+    //             url: "./../php/add_neverHaveIEver.php",
+    //             type: "POST",
+    //             data: { suggestion: text }
+    //         }).done(function(result) {
+    //             console.log(result);
+    //         });
 
-    $(".btn_no").bind({
-        click: function(e) {
-            var text = this.parentNode.children[0].innerHTML;
+    //         this.parentNode.remove();
+    //     }
+    // });
 
-            $.ajax({
-                url: "./../php/remove_suggestion_neverHaveIEver.php",
-                type: "POST",
-                data: { suggestion: text }
-            }).done(function(result) {
-                //console.log(result);
-            });
+    // $(".btn_no").bind({
+    //     click: function(e) {
+    //         var text = this.parentNode.children[0].innerHTML;
 
-            this.parentNode.remove();
-        }
-    });
+    //         $.ajax({
+    //             url: "./../php/remove_suggestion_neverHaveIEver.php",
+    //             type: "POST",
+    //             data: { suggestion: text }
+    //         }).done(function(result) {
+    //             console.log(result);
+    //         });
+
+    //         this.parentNode.remove();
+    //     }
+    // });
 });
+
+function accept_suggestion(suggestion) {
+    $.ajax({
+        url: "./../php/add_neverHaveIEver.php",
+        type: "POST",
+        data: { suggestion: suggestion }
+    }).done(function(result) {
+        console.log(result);
+    });
+}
+
+function reject_suggestion(suggestion) {
+    $.ajax({
+        url: "./../php/remove_suggestion_neverHaveIEver.php",
+        type: "POST",
+        data: { suggestion: suggestion }
+    }).done(function(result) {
+        console.log(result);
+    });
+}
