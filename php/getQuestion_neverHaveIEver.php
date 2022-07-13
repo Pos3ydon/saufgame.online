4cea652d27434d2440e3bc4b4a0fcf8a87c8a846
Joel Fischnaller <xyolomc@gmail.com>
2022-07-13 18:17:35 +0200

    Random Text PHP site working
    Fixed The problem at The random Text PHP site.

---
+++ php/getQuestion_neverHaveIEver.php
@@ -0,0 +1,31 @@
+<?php
+    $servername = "localhost";
+    $dbname = "saufgame";
+    $username = "root";
+    $password = "";
+    
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+    } catch(PDOException $e) {
+    //   echo "Connection failed: " . $e->getMessage();
+    }
+
+    try {
+        $stmt = $conn->prepare("select question from neverHaveIEver order by rand() limit 1");
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // print_r($result);
+
+        echo $result[0]['question'];
+
+        exit();
+
+
+        //echo "\nInserted successfully";
+    } catch(Exception $e) {
+        echo "\nInsert failed: " . $e->getMessage();
+    }
+?>
