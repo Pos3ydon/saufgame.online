7ebc9e37630850ed2fd9fc3a0579f9c83fa6980a
XDGamerLPLPLP <92158010+XDGamerLPLPLP@users.noreply.github.com>
2022-10-10 00:06:07 +0200

    adsa

--- js/footer.js
+++ js/footer.js
@@ -1,20 +1,29 @@
 $(document).ready(function(){
 
-    const isMobile = window.matchMedia('only screen and (max-width: 427px)').matches;
+    
+    const isMobile = window.matchMedia('only screen and (max-width: 768px)').matches;
+    
     if(isMobile == true){
-        $("#footer").load();
-    }else if(isMobile == false){
-        var scrolledUp = false;
-        window.addEventListener("wheel", function(e) {
-
-            if (e.wheelDeltaY < 0 && scrolledUp == false) {
-                $("#footer").animate({top: "-=" + $("#footer-container").height() + "px"}, 500);
-                scrolledUp = true;
-            }
-            else if (e.wheelDeltaY > 0 && scrolledUp == true) {
-                $("#footer").animate({top: "100%"}, 500);
-                scrolledUp = false;
-            }
-        }, {passive: false});
+        fetch("./footer.html")
+            .then(response => {
+                return response.text()
+            })
+            .then(data => {
+                document.querySelector("footer").innerHTML = data;
+            });
+        return;
     }
+    
+    var scrolledUp = false;
+    window.addEventListener("wheel", function(e) {
+
+        if (e.wheelDeltaY < 0 && scrolledUp == false) {
+            $("#footer").animate({top: "-=" + $("#footer-container").height() + "px"}, 500);
+            scrolledUp = true;
+        }
+        else if (e.wheelDeltaY > 0 && scrolledUp == true) {
+            $("#footer").animate({top: "100%"}, 500);
+            scrolledUp = false;
+        }
+    }, {passive: false});
 });  
