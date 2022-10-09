9437d24ff6a9818e9d8611e29b59b168bdb09b98
XDGamerLPLPLP <92158010+XDGamerLPLPLP@users.noreply.github.com>
2022-10-10 00:08:48 +0200

    Back to normal

--- js/footer.js
+++ js/footer.js
@@ -1,19 +1,4 @@
 $(document).ready(function(){
-
-    
-    const isMobile = window.matchMedia('only screen and (max-width: 427px)').matches;
-    console.log(isMobile);
-    if(isMobile == true){
-        fetch("./footer.html")
-            .then(response => {
-                return response.text()
-            })
-            .then(data => {
-                document.querySelector("footer").innerHTML = data;
-            });
-        return;
-    }
-
     var scrolledUp = false;
     window.addEventListener("wheel", function(e) {
 
