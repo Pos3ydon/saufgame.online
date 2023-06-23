0016754a6c5246c29a82ff2c530afbeae6daa333
Joel Fischnaller <xyolomc@gmail.com>
2023-06-23 12:27:48 +0200

    SQL Changes

--- php/add_suggestion_neverHaveIEver.php
+++ php/add_suggestion_neverHaveIEver.php
@@ -1,10 +1,10 @@
 <?php
     die;
 
-    $servername = "localhost";
-    $dbname = "saufgame";
-    $username = "root";
-    $password = "";
+    $servername = "rdbms.strato.de";
+    $dbname = "dbs11180804";
+    $username = "dbu5587866";
+    $password = "B3NnY.2012._.";
     
     try {
       $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
