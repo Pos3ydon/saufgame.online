fc5f183603170903728881f5ba266d6f75168b76
Joel Fischnaller <xyolomc@gmail.com>
2023-08-04 15:27:06 +0200

    Update getRecord_truthOrDare.php

--- php/getRecord_truthOrDare.php
+++ php/getRecord_truthOrDare.php
@@ -16,23 +16,36 @@
 
     try {
         $stmt = $conn->prepare("SELECT * FROM truthOrDare WHERE type = ? ORDER BY rand() LIMIT 1");
-        $stmt->execute([$_POST["type"]]);
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt->execute([$_POST["type"]]);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        // print_r($result);
+    if (!empty($result)) {
+        $row = $result[0];
+        $line = array_values($row); // Get only the values of the associative array
+        echo implode(",", $line); // Join the values with a comma and echo the whole line
+    } else {
+        echo "No data found."; // Or handle the case when no data is found
+    }
 
-        echo $result[0]['id'];
-        echo ",";
-        echo $result[0]['content'];
+    $stmt = $conn->prepare("SELECT count(id) AS count FROM truthOrDare WHERE type = ? ");
+    $stmt->execute([$_POST["type"]]);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        $stmt = $conn->prepare("SELECT count(id) AS count FROM truthOrDare WHERE type = ? ");
-        $stmt->execute([$_POST["type"]]);
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+    if (!empty($result)) {
         echo ",";
         echo $result[0]['count'];
+    } else {
+        echo ",0"; // Or handle the case when count is not available
+    }
 
-        exit();
+    exit();
+Hier haben wir array_values($row) verwendet, um nur die Werte des assoziativen Arrays zu erhalten, und dann implode(",", $line) verwendet, um die Werte mit einem Beistrich zu verbinden und die gesamte Zeile auszugeben. Wenn keine Daten gefunden werden, wird eine entsprechende Nachricht ausgegeben, um damit umzugehen. Gleiches gilt für den Fall, wenn die Anzahl der Zeilen nicht verfügbar ist.
+
+
+
+
+
+
 
 
         //echo "\nInserted successfully";
