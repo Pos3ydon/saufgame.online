4e5911d4d66478feaa398c7dfd4d4afca4169d99
Joel Fischnaller <92158010+fishenjoe@users.noreply.github.com>
2023-10-03 12:15:55 +0200

    Fixed Mobile php Bug
    PHP hat die antwort nicht in die datenbank gelesen

--- php/add_suggestion_neverHaveIEver.php
+++ php/add_suggestion_neverHaveIEver.php
@@ -1,12 +1,5 @@
 <?php
 
-    session_start();
-        
-    if (!isset($_SESSION["user"])) {
-        header("Location: ./../../index.html");
-        exit;
-    }
-
     $servername = "rdbms.strato.de";
     $dbname = "dbs11180804";
     $username = "dbu5587866";
